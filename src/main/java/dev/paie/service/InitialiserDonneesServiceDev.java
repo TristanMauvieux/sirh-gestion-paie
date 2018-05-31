149331c33ac44651ab187d275ba81a619d5fd67b
Tristan Mauvieux <mauvieux.tristan@gmail.com>
2018-05-31 12:33:24 +0200

    web security

--- src/main/java/dev/paie/service/InitialiserDonneesServiceDev.java
+++ src/main/java/dev/paie/service/InitialiserDonneesServiceDev.java
@@ -12,6 +12,7 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +21,8 @@ import dev.paie.entite.Entreprise;
 import dev.paie.entite.Grade;
 import dev.paie.entite.Periode;
 import dev.paie.entite.ProfilRemuneration;
+import dev.paie.entite.Utilisateur;
+import dev.paie.entite.Utilisateur.ROLES;
 
 @Service
 public class InitialiserDonneesServiceDev implements InitialiserDonneesService {
@@ -28,20 +31,29 @@ public class InitialiserDonneesServiceDev implements InitialiserDonneesService {
 	@Autowired
 	private EntityManager em;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Override
 	@Transactional
 	public void initialiser() {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("cotisations-imposables.xml",
 				"cotisations-non-imposables.xml", "entreprises.xml", "grades.xml", "profils-remuneration.xml");
-
+		// recupère les cotisations dans les fichiers XML
 		Map<String, Cotisation> ctos = context.getBeansOfType(Cotisation.class);
 		Collection<Cotisation> cotis = ctos.values();
+		// Récupère les entreprises dans les fichier XML
 		Map<String, Entreprise> entos = context.getBeansOfType(Entreprise.class);
 		Collection<Entreprise> entis = entos.values();
+		// Récupère les grades dans les fichier XML
 		Map<String, Grade> grados = context.getBeansOfType(Grade.class);
 		Collection<Grade> gradis = grados.values();
+		// récupère les ProfilRémuneration (équivalent des employer), dans le
+		// fichier XML
 		Map<String, ProfilRemuneration> proRemLis = context.getBeansOfType(ProfilRemuneration.class);
 		Collection<ProfilRemuneration> proRemColl = proRemLis.values();
+
+		// Envoi tous en BDD
 		for (Cotisation cotisation : cotis) {
 			em.persist(cotisation);
 		}
@@ -54,7 +66,7 @@ public class InitialiserDonneesServiceDev implements InitialiserDonneesService {
 		for (ProfilRemuneration prof : proRemColl) {
 			em.persist(prof);
 		}
-
+		// création de douze périodes qui représente les douzes mois de l'année
 		List<Periode> annee = new ArrayList<>();
 		int anneeCourante = LocalDate.now().getYear();
 		for (int i = 1; i <= 12; i++) {
@@ -66,6 +78,34 @@ public class InitialiserDonneesServiceDev implements InitialiserDonneesService {
 
 		for (Periode p : annee) {
 			em.persist(p);
+		}
+
+		// création d'utilisateur avec diffrent profil, 1 Admin 2 User
+
+		List<Utilisateur> listMembre = new ArrayList<>();
+		Utilisateur admin = new Utilisateur();
+		admin.setEstActif(true);
+		admin.setNomUtilisateur("holt zark");
+		admin.setMotDePasse(passwordEncoder.encode("zark"));
+		admin.setRole(ROLES.ROLE_ADMINISTRATEUR);
+
+		listMembre.add(admin);
+
+		Utilisateur user1 = new Utilisateur();
+		user1.setEstActif(true);
+		user1.setMotDePasse(passwordEncoder.encode("user"));
+		user1.setNomUtilisateur("User");
+		user1.setRole(ROLES.ROLE_UTILISATEUR);
+		Utilisateur user2 = new Utilisateur();
+		user2.setEstActif(false);
+		user2.setMotDePasse(passwordEncoder.encode("hallfinder"));
+		user2.setNomUtilisateur("sword");
+		user2.setRole(ROLES.ROLE_UTILISATEUR);
+		listMembre.add(user1);
+		listMembre.add(user2);
+
+		for (Utilisateur u : listMembre) {
+			em.persist(u);
 		}
 
 		context.close();
---
+++ src/main/webapp/WEB-INF/views/connexion.jsp
@@ -0,0 +1,31 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Paie - App</title>
+        <link rel="stylesheet" href='<c:url value="/bootstrap-3.3.7-dist/css/bootstrap.min.css"></c:url>'>
+    </head>
+    <body class="container">        
+
+        <h1>Connexion</h1>
+
+        <!-- Spring Security s'attend aux paramètres "username" et "password" -->
+        <form method="post">
+            <input name="username" placeholder="Username" class="form-control">
+            <input name="password" placeholder="password" class="form-control">
+            <input type="submit" value="Se connecter" class="btn btn-danger">
+        
+        <sec:csrfInput/>
+        </form>
+
+        <!-- en cas d'erreur un paramètre "error" est créé par Spring Security -->
+        <c:if test="${param.error !=null}">
+            Erreur d'authentification
+        </c:if>
+        
+        <script src="/bootstrap-3.3.7-dist/js/bootstrap.min.js" />
+    </body>
+</html>
